055c4970bda171ac885a1b1babfd1e02af96545c
Alisson Machado <39538873+alissoncmachado@users.noreply.github.com>
2022-05-12 00:07:20 -0300

    Atividades atualizadas

--- 2022-1/ALGORITMOS I/C/AULA 6/EX03/Exercicio03.c
+++ 2022-1/ALGORITMOS I/C/AULA 6/EX03/Exercicio03.c
@@ -9,9 +9,9 @@ int main(){
     scanf("%f", &numero2);
 
     if (numero1 < numero2) { 
-        printf("O número %.2f é maior.\n", numero2);
+        printf("O número %.2f é menor.\n", numero1);
     } else if (numero2 < numero1){
-        printf("O número %.2f é maior.\n", numero1);
+        printf("O número %.2f é menor.\n", numero2);
     } else {
          printf("Os números são iguais!\n");       
     }
