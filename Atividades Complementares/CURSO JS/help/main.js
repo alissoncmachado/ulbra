6d59f6ef6f6b75789e26b90cfd3fa88d4e269313
Alisson Machado <39538873+alissoncmachado@users.noreply.github.com>
2022-06-03 17:51:19 -0300

    Atividades WEB e IAC

--- Atividades Complementares/CURSO JS/help/main.js
+++
@@ -1,6 +0,0 @@
-var idade = 18
-if (idade < 18) {
-    console.log('Você é menor idade, por isso não pode votar')
-} else if (idade >=16 && idade <= 18) {
-    console.log ('Você pode votar, pois para pessoas entre 16 e 18, o voto é opcional')
-    }
